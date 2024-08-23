13d4a151c6030a6b750fa2f035a03ffb2a4459ad
fandion13 <fandion13@gmail.com>
2024-08-23 15:35:41 -0300

    Criação de solution para o vs e utilização de connection string do appsettings

--- myfinance-web-dotnet-infra/MyFinanceDBContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDBContext.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet_infra;
 
 public class MyFinanceDBContext : DbContext
 {
+    private readonly IConfiguration _configuration;
+
     // Camada utilizada para conexão com o DB
     public DbSet<PlanoConta> PlanoConta { get; set; }
     public DbSet<Transacao> Transacao { get; set; }
 
+    public MyFinanceDBContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-       optionsBuilder.UseSqlServer(@"Server=FILIPE;Database=myfinance;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False;");
+        // Para utilizar a connection string do bd que está no appsettings
+        var connectionString = _configuration.GetConnectionString("Database");
+        optionsBuilder.UseSqlServer(connectionString);
+
+        // Conexão local confiável hard coded
+        //optionsBuilder.UseSqlServer(@"Server=FILIPE;Database=myfinance;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=False;");
+
+        // Conexão de db virtual hard coded
+        // optionsBuilder.UseSqlServer(@"Server=meuservidorsqlserver.database.windows.net;Database=myfinance;User Id=user;Password=myPassword");
     }
 }
